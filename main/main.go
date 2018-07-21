ecb317454414ee5b3e61242695ae7049e3d3654e
Yuri <yuri.tayama@linksports.co.jp>
2018-07-22 00:03:58 +0900

    initial commit

---
+++ main/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gophercises/urlshort"
+)
+
+func main() {
+	// mux:マルチプレクサ(複数の信号を受け取り、１つの信号にして返却する)
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string {
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	
+	// Build the YAMLHandler using the mapHandler as the fallback
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+// ServeMux: HTTPのマルチプレクサ
+//リクセストされたURLと登録されたパターンのリストを照合し、URLに最も近いパターンのハンドラーを呼び出します。
+// より長いパターンが優先される
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+// ResponseWriter: HTTPのレスポンスを構築するために使用
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
